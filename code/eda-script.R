e4f2df6b829494320c506b8554e45bde0e5ad834
nikkirethyo <nikkireth@berkeley.edu>
2016-10-04 22:30:40 -0700

    added code for regression statistics

---
+++ code/eda-script.R
@@ -0,0 +1,16 @@
+#read data into R
+data <- read.csv("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw02/data/Advertising.csv")
+
+#apply simple linear regression model 
+lm <- lm(formula=Sales ~ TV, data=data)
+
+#extract regression coefficients info 
+table1 <- round(summary(lm)$coefficients, 2)
+
+#extract regression quality indices 
+RSE <- round(summary(lm)$sigma, 2)
+RR <- round(summary(lm)$r.squared, 2) 
+Fstat <- round(summary(lm)$fstatistic[[1]], 2)
+table2 <- matrix(c(RSE,RR,Fstat), nrow=3, ncol=1)
+rownames(table2) <- c("Residual Std Error", "R Squared", "F statistic")
+colnames(table2) <- c( "Value")
