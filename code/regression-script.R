44e5a3b136911f33e6af05fc0eb4834a9e34d26c
nikkirethyo <nikkireth@berkeley.edu>
2016-10-07 16:40:57 -0700

    updated regression script

--- code/regression-script.R
+++ code/regression-script.R
@@ -6,9 +6,18 @@ regression <- lm(formula=Sales ~ TV, data=data)
 
 #summarize the regression 
 sum <- summary(regression) 
+coeff <- sum$coefficients
 
 #gather residuals from the regression
 res <- resid(regression)
+
+#regression quality indices
+RSE <- round(sum$sigma, 2)
+RR <- round(sum$r.squared, 2)
+Fstat <- round(sum$fstatistic[[1]], 2)
+table2 <- matrix(c(RSE,RR,Fstat), nrow=3, ncol=1)
+rownames(table2) <- c("Residual Std Error", "R Squared", "F statistic")
+colnames(table2) <- c( "Value")
 
 #plot the scatterplot with regression line, save as png and pdf
 png("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw02/images/scatterplot-tv-sales.png")
@@ -24,5 +33,5 @@ dev.off()
 
 #save binary rdata file to data directory 
 setwd("/Users/nrethmeier/Documents/stat159/stat159-fall2016-hw02/data")
-save(data, regression, sum, res, file="regression.Rdata")
+save(data, regression, sum, res, coeff, table2, file="regression.Rdata")
 
